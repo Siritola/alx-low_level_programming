7049e736bc01689734abd45eb1776e681bb09d9e
Itoro Ekpene <itoroekpene27@gmail.com>
2023-03-13 13:03:14 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,9 +5,9 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grid(int **grid, int height);
+int **alloc_grid(int **width, int height);
 void free_grid(int **grid, int height);
-char *argdtostr(int ac, char **av);
+char *argstostr(int ac, char **av);
 
 #endif
 
