73b26ddd4c1127e1a30049b8714416749345f99c
Itoro Ekpene <itoroekpene27@gmail.com>
2023-03-14 06:59:57 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,16 +7,14 @@
  * @grid: grid to free
  * @height: height of grid
  */
-
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height == 0)
-		return;
-
 	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
+	}
 
 	free(grid);
 }
